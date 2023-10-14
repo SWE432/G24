debc2739dad8507f6bfc51a3986aad02efdf882b
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2023-10-13 20:47:22 -0400

    created iteration 3 folder; producer changes: added event handlers for changing pages and for submission

---
+++ iteration3/producer/index.js
@@ -0,0 +1,81 @@
+
+
+// finds selected value from group of radio-inputs
+// then displays it through an alert
+function alertValue(category) {
+    let categoryList = document.getElementsByName(category);
+    let selectedValue = null;
+
+    for (let i = 0; i < categoryList.length; i++) {
+        let currElement = categoryList[i];
+        if (currElement.checked) {
+            selectedValue = currElement.value;
+            break;
+        }
+    }
+
+    if (selectedValue !== null) {
+        alert(`Selected ${category}: ${selectedValue}`);
+    }
+    else {
+        alert(`No ${category} selected`)
+    }
+};
+
+
+// finds selected values from group of check boxes
+// then displays them through an alert
+function alertList(category) {
+    let categoryList = document.getElementsByName(category);
+    let selectedValues = [];
+
+    for (let i = 0; i < categoryList.length; i++) {
+        let currElement = categoryList[i];
+        if (currElement.checked) {
+            selectedValues.push(currElement.value);
+        }
+    }
+
+    if (selectedValues.length > 0) {
+        alert(`Selected ${category}s: ${selectedValues.join(", ")}`);
+    } else {
+        alert(`No ${category}s selected!`);
+    }
+};
+
+
+
+function submitPlaylist(event) {
+    alertValue('playlist');
+    event.preventDefault();
+}
+
+
+function submitEvents(event) {
+    alertList('event');
+    event.preventDefault();
+}
+
+
+function submitDJ(event) {
+    alertValue('DJ');
+    event.preventDefault();
+}
+
+
+function submitTimeslot(event) {
+    alertValue('timeslot');
+    event.preventDefault();
+}
+
+
+
+const playlistForm = document.getElementById("playlist-form");
+const eventsForm = document.getElementById("events-form");
+const djForm = document.getElementById("dj-form");
+const timeslotForm = document.getElementById("timeslot-form");
+
+playlistForm && playlistForm.addEventListener("submit", submitPlaylist);
+eventsForm && eventsForm.addEventListener("submit", submitEvents);
+djForm && djForm.addEventListener("submit", submitDJ);
+timeslotForm && timeslotForm.addEventListener("submit", submitTimeslot);
